88dfe6dd9ea531a53695cf88c3eb14abb88caac4
JLP-55 <jack.v.pekin@gmail.com>
2024-03-01 12:24:05 +1100

    Tables are being created using sequelize...almost. Need to ascertain the exact nature of the relationship between product, tag and product tag. Still to do: see comments in product.js on setting default values. Otherwise, should work.

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,36 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Needs to validate the value is a decimal.
+      // Also, the documentation states that "type: DataTypes.DECIMAL(11,10)" whatever that means.
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Sets a default value
+      // @Default(10),
+      // Needs to validate the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      referneces: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
