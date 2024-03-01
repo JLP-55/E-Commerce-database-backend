91fc54fc05b2686163c97dee763227ea3d542c1b
JLP-55 <jack.v.pekin@gmail.com>
2024-03-01 15:20:13 +1100

    basic routing complete GET requests for 'all' and ':id' work, except for tagData, as there is no relationship set up yet. Still have to set defaults and ensure validation of DataTypes as decimal in models, as well as define relationships 'through' ProductTag for Products and Tag models

--- models/index.js
+++ models/index.js
@@ -4,24 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// - for the more info on one-to-many relationships see day-3 class video, @36:30
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-})
+});
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-})
+});
 
-// Products belongToMany Tags (through ProductTag)
-Product.hasMany(Tag, {
-  foreignKey: "product_id",
-})
+// // Products belongToMany Tags (through ProductTag)
+// Product.hasMany(Tag, {
+//   foreignKey: "product_id",
+// });
 
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  foreignKey: "product_id",
-})
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   foreignKey: "product_id",
+// });
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({message: "error"});
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, /*{model: Tag}*/],
+    });
+    if (!productData) {
+      res.status(404).json({message: "no such product exists"});
+      return;
+    };
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({message: "error"});
+  }
   // be sure to include its associated Category and Tag data
 });
 
