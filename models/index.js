d37185914766c7833afb1642ffed7bebf2c9edb1
JLP-55 <jack.v.pekin@gmail.com>
2024-03-03 16:08:36 +1100

    Had to link the Tag and Products models through ProductTag using a foreign key.

--- models/index.js
+++ models/index.js
@@ -9,33 +9,31 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE"
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
+  onDelete: "CASCADE"
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  /*foreignKey: "product_id", constraints: false*/
+  foreignKey: "product_id",
 
   through: {
     model: ProductTag,
-    unique: false,
   },
-  as: "random_alias"
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  /*foreignKey: "product_id", constraints: false*/
+  foreignKey: "tag_id",
 
   through: {
     model: ProductTag,
-    unique: false,
   },
-  as: "random_alias"
 });
 
 module.exports = {
